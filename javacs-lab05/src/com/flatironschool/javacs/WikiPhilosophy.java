7e92f91a84fdc9a973747bb2e76cb3effd78ea67
angulostephanie <angulostephanie21@gmail.com>
2016-07-18 00:27:19 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -30,22 +30,33 @@ public class WikiPhilosophy {
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
+        String baseUrl = "https://en.wikipedia.org";
+        String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+        int count = 0;
+        while (!url.equals("https://en.wikipedia.org/wiki/Philosophy") && count < 15) {
+            System.out.println("Visiting: " + url);
+            Elements paragraphs = wf.fetchWikipedia(url);
+            Element firstPara = paragraphs.get(0);
+            Iterable<Node> iter = new WikiNodeIterable(firstPara);
+            boolean hasLink = false;
+            for (Node node : iter) {
+                if (node.hasAttr("href")) {
+                    url = baseUrl + node.attr("href");
+                    hasLink = true;
+                    break;
+                }
+            }
+            if (!hasLink) {
+                break;
+            }
+            count++;
+         }
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
         }
 
         // the following throws an exception so the test fails
         // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
-}
+        //String msg = "Complete this lab by adding your code and removing this statement.";
+       // throw new UnsupportedOperationException(msg);
+	   }
+
